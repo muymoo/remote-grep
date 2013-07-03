358cfa791599f14ab0c483d0b16df1b11d9b1a90
Evan Bowling <evan.j.bowling@gmail.com>
2013-07-03 02:06:18 -0500

    added new skeleton class for DistributedFileSys

---
+++ remote-grep/src/org/uiuc/cs/distributed/grep/DistributedFileSystem.java
@@ -0,0 +1,29 @@
+package org.uiuc.cs.distributed.grep;
+
+public class DistributedFileSystem {
+
+	public DistributedFileSystem()
+	{
+		
+	}
+	
+	public boolean put(String localFile)
+	{
+		return false;
+	}
+	
+	public String get()
+	{
+		return "";
+	}
+	
+	public boolean delete(String remoteFileName)
+	{
+		return false;
+	}
+	
+	private String hashLocalFileName(String localFileName)
+	{
+		return "";
+	}
+}
